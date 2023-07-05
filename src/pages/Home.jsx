68a4e717e0ad76806436a9ec77a7d3271f7c273b
Rajdip Sinh <rajdips834@gmail.com>
2023-07-06 02:04:08 +0530

    Type done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,32 @@
 import React from "react";
-import Typewriter from "typewriter-effect";
 import "./fonts.css";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 export default function Home() {
+  const [word] = useTypewriter({
+    words: ["Life!", "Money!", "Time!"],
+    loop: true,
+    typeSpeed: 120,
+    deleteSpeed: 100,
+    cursorStyle: "|",
+  });
   return (
-    <div className="Hero-text">
-      <div></div>
-      <Typewriter
-        options={{ loop: true, delay: "natural", autoStart: true }}
-        onInit={(typewriter) => {
-          typewriter
-            .typeString("At Healify we save Lives!")
-            .start()
-            .pauseFor(1000)
-            .deleteChars(6)
-            .typeString("Money!")
-            .pauseFor(1000)
-            .deleteChars(6)
-            .typeString("Time!");
-        }}
-      ></Typewriter>
+    <div>
+      {/*Typewriter */}
+      <div
+        className=" @apply flex pt-60 pl-4 flex-col shrink-0 text-[#2C0472] text-[64px] not-italic font-normal leading-[normal];
+      font-family: Lato;"
+      >
+        <h1 className="Hero-text">At Healify we save</h1>
+        <span
+          className="@apply flex w-[100vh] h-[40vh] flex-col shrink-0 text-[#2C0472] text-[64px] not-italic font-normal leading-[normal];
+       font-bold;"
+        >
+          Your {word}
+        </span>
+      </div>
+
+      {/*book doctors */}
+      <div className=""></div>
     </div>
   );
 }
