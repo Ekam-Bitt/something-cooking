dc710d66c88e5fceac2cad2dadc183889350fca6
Rajdip Sinh <rajdips834@gmail.com>
2023-07-05 21:06:25 +0530

    initial admin page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import Typewriter from "typewriter-effect";
+import "./fonts.css";
 export default function Home() {
   return (
-    <div>
-      <div>
-        <img src="https://img.freepik.com/free-vector/build-your-home-concept-illustration_114360-5623.jpg?w=1480&t=st=1687985232~exp=1687985832~hmac=9d778ff6f45003c8dd1b6eccf1b67f1015b86f766f06c0950a4f7c43299a8e05" alt="" />
-      </div>
+    <div className="Hero-text">
+      <div></div>
+      <Typewriter
+        options={{ loop: true, delay: "natural", autoStart: true }}
+        onInit={(typewriter) => {
+          typewriter
+            .typeString("At Healify we save Lives!")
+            .start()
+            .pauseFor(1000)
+            .deleteChars(6)
+            .typeString("Money!")
+            .pauseFor(1000)
+            .deleteChars(6)
+            .typeString("Time!");
+        }}
+      ></Typewriter>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "violet-2": "#2C0472",
+      },
+    },
   },
-  plugins: [require('@tailwindcss/forms'),],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
