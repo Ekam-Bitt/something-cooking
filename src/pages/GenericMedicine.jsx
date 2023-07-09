8cb7725fd806d373b7cfd78519e8c74f7d039da8
soumi guria <soumiguria.cse2022@nsec.ac.in>
2023-07-10 04:23:36 +0530

    Server side working

--- src/pages/GenericMedicine.jsx
+++ src/pages/GenericMedicine.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+// import { cleanup } from '@testing-library/react';
+// import React, { useEffect } from 'react'
+
+// export default function GenericMedicine() {
+
+//   let API = "http://localhost:3065/"
+
+//   const fetchApiData = async (url) => {
+//     try {
+//       const res = fetch(url);
+//     } catch (error) {
+//       console.log(error); 
+//     }
+//   }
+
+//   useEffect(() => {
+//     fetchApiData(API);
+//   },[]);
+
+//   return (
+//     <div>GenericMedicine</div>
+//   )
+// }
+
+
+
+
+
+
+import { cleanup } from '@testing-library/react';
+import React, { useEffect } from 'react';
 
 export default function GenericMedicine() {
+  let API = "http://localhost:3065/";
+
+  const fetchApiData = async (url) => {
+    try {
+      const res = await fetch(url); // Await the fetch call
+      // Process the response as needed
+    } catch (error) {
+      console.log(error); 
+    }
+  };
+
+  useEffect(() => {
+    fetchApiData(API);
+  }, []);
+
   return (
     <div>GenericMedicine</div>
-  )
+  );
 }
